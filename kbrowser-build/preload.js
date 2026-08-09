a19ff35d136effe8a22db321ea772720ebdbee16
JGI <149709655+JKI950@users.noreply.github.com>
2026-08-09 14:34:13 +0900

    Expose KBrowser page save, capture and credential APIs

--- kbrowser-build/preload.js
+++ kbrowser-build/preload.js
@@ -1,5 +1,11 @@
 'use strict';
 const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('kbrowserHost', {
-  onOpenUrl: (callback) => ipcRenderer.on('open-url-in-tab', (_event, url) => callback(url))
+  onOpenUrl: (callback) => ipcRenderer.on('open-url-in-tab', (_event, url) => callback(url)),
+  getCredential: (url) => ipcRenderer.invoke('credential-get', url),
+  saveCredential: (url, username, password) => ipcRenderer.invoke('credential-save', url, username, password),
+  saveCompletePage: (contentsId) => ipcRenderer.invoke('save-complete-page', contentsId),
+  startCapture: (contentsId) => ipcRenderer.invoke('capture-start', contentsId),
+  stopCapture: (contentsId) => ipcRenderer.invoke('capture-stop', contentsId),
+  isRecording: (contentsId) => ipcRenderer.invoke('capture-is-recording', contentsId)
 });
